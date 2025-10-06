d144f8f688759614a468b58ee48837843ee6a672
Pooja Sri Putsala <98636476+Putsala1712@users.noreply.github.com>
2025-10-06 12:10:24 +0530

    Update fifo_axi.v

--- fifo_axi.v
+++ fifo_axi.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11.09.2025 11:40:06
-// Design Name: 
-// Module Name: fifo_axi
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module fifo_axi #(parameter DATA_WIDTH=32, FIFO_DEPTH=16)(
 //Global Signals
